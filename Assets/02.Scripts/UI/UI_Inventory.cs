32a5478f509e3dd66a834bc41de2675e06a6ac05
MirDragon40 <mirjeong6463@gmail.com>
2024-04-16 16:54:48 +0900

    아이템 인벤토리 수정중 & 손전등

--- Assets/02.Scripts/UI/UI_Inventory.cs
+++ Assets/02.Scripts/UI/UI_Inventory.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Inventory : MonoBehaviour
 {
     public List<ItemObject> itemObjects; // 아이템 리스트
-    public Image[] SlotImages; // 인벤토리 슬롯의 이미지 배열
+    public UI_InventorySlot[] Slots; // 인벤토리 슬롯의 이미지 배열
 
     private void Start()
     {
@@ -16,10 +17,17 @@ public class UI_Inventory : MonoBehaviour
 
     private void InitInventory()
     {
-        // 아이템 리스트와 UI 슬롯 이미지 연결
-        for (int i = 0; i < SlotImages.Length; i++)
+        for (int i = 0; i < Slots.Length; i++)
         {
-
+            if (i < itemObjects.Count)
+            {
+                Slots[i].SetItem(itemObjects[i]); // 아이템 리스트의 각 아이템을 슬롯에 설정
+            }
+            else
+            {
+                // 아이템 리스트에 해당 슬롯을 채울 아이템이 없다면, 빈 아이템을 설정
+                Slots[i].SetItem(new ItemObject { ItemType = ItemType.None });
+            }
         }
     }
 }
---
+++ Assets/02.Scripts/UI/UI_InventorySlot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_InventorySlot : MonoBehaviour
+{
+    public GameObject[] itemImages; // 아이템 타입에 따른 이미지 오브젝트 배열
+
+    // 슬롯에 아이템을 설정하는 메서드
+    public void SetItem(ItemObject item)
+    {
+        // 우선 모든 자식 이미지를 비활성화합니다.
+        foreach (var image in itemImages)
+        {
+            image.SetActive(false);
+        }
+
+        if ((int)item.ItemType == (int)ItemType.None)
+        {
+            // 아이템 타입이 None이면, 어떤 이미지도 표시하지 않는다.
+            return;
+        }
+
+        // 아이템 타입에 맞는 이미지를 활성화
+        itemImages[(int)item.ItemType - 1].SetActive(true);
+    }
+}
